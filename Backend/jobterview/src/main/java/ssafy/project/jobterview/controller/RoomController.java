6dcfd58be184d3245b30e5b68e027121254e9927
TAEHEE JEONG <th1563@naver.com>
2023-07-19 10:19:45 +0900

    refactor - Room Controller 수정

--- Backend/jobterview/src/main/java/ssafy/project/jobterview/controller/RoomController.java
+++ Backend/jobterview/src/main/java/ssafy/project/jobterview/controller/RoomController.java
@@ -1,10 +1,6 @@
 package ssafy.project.jobterview.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
