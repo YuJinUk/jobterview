0e93b1d99f75c2bb29dc22a65ec6c16145c1c29f
박대균 <hoyungss123@naver.com>
2023-07-24 17:06:58 +0900

    feat-add socail login

--- Backend/jobterview/src/main/java/ssafy/project/jobterview/config/SecurityConfig.java
+++ Backend/jobterview/src/main/java/ssafy/project/jobterview/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package ssafy.project.jobterview.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -16,27 +18,54 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import ssafy.project.jobterview.config.auth.PrincipalDetailService;
+import ssafy.project.jobterview.config.auth.*;
 
 import java.util.List;
 
 @Configuration
 @EnableWebSecurity //스프링 시큐리티 필터 등록
+//@EnableOAuth2Clien
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
 
     private PrincipalDetailService principalDetailService;
+    private PrincipalOauth2UserService principalOauth2UserService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;;
 
     @Autowired
-    public SecurityConfig(PrincipalDetailService principalDetailService) {
+    public SecurityConfig( PrincipalDetailService principalDetailService,
+                          @Lazy PrincipalOauth2UserService principalOauth2UserService,
+                          @Lazy BCryptPasswordEncoder bCryptPasswordEncoder
+    ) {
         this.principalDetailService = principalDetailService;
+        this.principalOauth2UserService = principalOauth2UserService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+
+    @Autowired
+    private CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+
+    @Autowired
+    private CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
+
+    @Autowired
+    private CustomLogoutSuccessHandler customLogoutSuccessHandler;
+
+
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean
     public BCryptPasswordEncoder encodePwd(){
         return new BCryptPasswordEncoder();
     }
+
 
     @Bean
     public CorsFilter corsFilter() {
@@ -48,6 +77,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
+
 
     // 시큐리티가 대신 로그인해주는데 password를 가로채는데
     // 해당 password가 뭘로 해쉬화해서 회원가입이 되었는지 알아야
@@ -78,19 +108,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/hello").permitAll()
-                .antMatchers("/api/login").permitAll()
-                .antMatchers("/api/join").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/join").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/logingo").permitAll()
-                .antMatchers("/api/admin/**").access("hasRole('admin')")
+                .antMatchers("/oauth2/**").permitAll()
+                .antMatchers(HttpMethod.POST,"/oauth2/**").permitAll()
+                .antMatchers(HttpMethod.DELETE,"/oauth2/**").permitAll()
+                .antMatchers(HttpMethod.PUT,"/oauth2/**").permitAll()
+                //.antMatchers(HttpMethod.POST,"/auth/logout").permitAll()
+                .antMatchers("/auth/ok").permitAll()
+                .antMatchers(HttpMethod.POST, "/member/join").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                .antMatchers("/admin/**").access("hasRole('admin')")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/api/login")
+                .loginPage("/auth/login")
                 .usernameParameter("email")
-                .loginProcessingUrl("/api/logingo")
-                .defaultSuccessUrl("/api/hello");
+                .loginProcessingUrl("/auth/login")
+                .successHandler(customAuthenticationSuccessHandler)
+                .failureHandler(customAuthenticationFailureHandler)
+                .and()
+                .logout()
+                .logoutUrl("/auth/logout") // 로그아웃 URL 설정
+                .logoutSuccessHandler(customLogoutSuccessHandler) // 로그아웃 성공 후 리다이렉트할 URL 설정
+                .invalidateHttpSession(true) // HTTP 세션 무효화
+                .deleteCookies("JSESSIONID") // 로그아웃 시 쿠키 삭제
+                .clearAuthentication(true)// 현재 인증 정보 삭제
+                .and()
+                        .oauth2Login()
+                        .successHandler(customAuthenticationSuccessHandler)
+                        .failureUrl("/auth/ok")		// 로그인 실패 시 /loginForm으로 이동
+                        .userInfoEndpoint()		// 로그인 성공 후 사용자정보를 가져온다
+                        .userService(principalOauth2UserService);	//사용자정보를 처리할 때 사용한다
 
         //http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
 
--- Backend/jobterview/src/main/java/ssafy/project/jobterview/config/auth/PrincipalDetail.java
+++ Backend/jobterview/src/main/java/ssafy/project/jobterview/config/auth/PrincipalDetail.java
@@ -1,20 +1,34 @@
 package ssafy.project.jobterview.config.auth;
 
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import ssafy.project.jobterview.domain.Member;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
-public class PrincipalDetail implements UserDetails {
+@Getter
+public class PrincipalDetail implements UserDetails, OAuth2User {
 
     private Member member;
+    private Map<String, Object> attributes;
+
 
     public PrincipalDetail(Member member){
-
         this.member =member;
     }
+
+    //OAuth2User : OAuth2 로그인 시 사용
+    public PrincipalDetail(Member member, Map<String, Object> attributes) {
+        //PrincipalOauth2UserService 참고
+        this.member = member;
+        this.attributes = attributes;
+    }
+
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -28,6 +42,10 @@ public class PrincipalDetail implements UserDetails {
         return collect;
     }
 
+//    public String getNickname() {
+//        return member.getNickname();
+//    }
+
     @Override
     public String getPassword() {
         return member.getPassword();
@@ -37,6 +55,9 @@ public class PrincipalDetail implements UserDetails {
     public String getUsername() {
         return member.getEmail();
     }
+
+
+
 
     @Override
     public boolean isAccountNonExpired() {
@@ -56,5 +77,26 @@ public class PrincipalDetail implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+
+
+    /**
+     * OAuth2User 구현
+     * @return
+     */
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    /**
+     * OAuth2User 구현
+     * @return
+     */
+    @Override
+    public String getName() {
+        String sub = attributes.get("sub").toString();
+        return sub;
     }
 }
