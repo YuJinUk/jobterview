bd46d84b96e6c1db4128acc84c49425776e8365e
lsw98 <sangwoo9808@gmail.com>
2023-07-27 16:39:39 +0900

    feat : 페이징 표시 로직 구현

--- Frontend/jobterview/src/router.js
+++ Frontend/jobterview/src/router.js
@@ -1,12 +1,16 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import NavBar from "./views/components/NavBar.vue";
+import MessageList from "./views/message/MessageList.vue";
 // import MainPage from "./views/MainPage.vue";
 
 const routes = [
     {
         path: '/',
         name: 'NavBar',
-        component: NavBar
+        components: {
+            default: MessageList,
+            header: NavBar,
+        }
     }
 ]
 
