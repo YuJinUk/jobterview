be575df47fdad08d191f72a2515b7331affb47e3
lsw98 <sangwoo9808@gmail.com>
2023-07-28 16:28:51 +0900

    fix : store init

--- Frontend/jobterview/src/api/messageApi.js
+++ Frontend/jobterview/src/api/messageApi.js
@@ -1,7 +1,9 @@
 import apiTest from "apiTest";
 
 // 수신 메시지 조회
-
+async function getReceivedMessage(nickname, success, fail) {
+    await apiTest.get(`/message/me?nickname=${nickname}`).then(success).catch(fail);
+}
 // 송신 메시지 조회
 
 // 수신 메시지 삭제
