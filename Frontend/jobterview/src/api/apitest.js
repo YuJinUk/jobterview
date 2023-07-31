36fa3d830527c94e1c282f50d8bac634444b84ec
TAEHEE JEONG <th1563@naver.com>
2023-07-31 17:57:27 +0900

    merge conflicts resolve

--- Frontend/jobterview/src/api/apitest.js
+++ Frontend/jobterview/src/api/apitest.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-// API 실제 사용 헤더 및 URL 설정
+// API 개발 사용 헤더 및 URL 설정
 export default axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:8080",
     headers: {
         "Content-Type": "application/json;charset=utf-8",
-        "Access-Control-Allow-Origin" : "*",
     },
 });
