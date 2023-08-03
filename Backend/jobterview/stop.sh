58f54d389064e176cc5a0a4a28cc7912762decbc
ksc <ksc036@gmail.com>
2023-08-03 16:30:35 +0900

    stop.sh 수정

--- Backend/jobterview/stop.sh
+++ Backend/jobterview/stop.sh
@@ -1,6 +1,6 @@
 process=$(sudo lsof -i :8060 | awk 'NR==2{print $2}')
 
-if [[ -n "$process" ]]; then
+if [ -n "$process" ]; then
     echo "포트 8060을 사용하는 프로세스($process)를 종료합니다."
     sudo kill "$process"
 else
