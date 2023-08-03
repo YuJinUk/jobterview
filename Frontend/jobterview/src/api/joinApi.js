6190e02658fa7978e7428d52879157d28bb38fff
lsw98 <sangwoo9808@gmail.com>
2023-08-03 10:02:04 +0900

    feat : FE 이메일 전송 API, 회원 가입시 이메일 전송

--- Frontend/jobterview/src/api/joinApi.js
+++ Frontend/jobterview/src/api/joinApi.js
@@ -11,7 +11,8 @@ async function checkEmail(email,success,fail){
 async function checkNickname(nickname,success,fail){
     await apiTest.get(`/member/nicknameCheck?nickname=${nickname}`,nickname).then(success).catch(fail);
 }
+async function sendEmail(email, success, fail) {
+    await apiTest.post(`/member/emailConfirm?email=${email}`).then(success).catch(fail);
+}
 
-
-
-export {join,checkEmail,checkNickname};
+export {join,checkEmail,checkNickname,sendEmail};
--- Frontend/jobterview/src/router/joinRouter.js
+++ Frontend/jobterview/src/router/joinRouter.js
@@ -3,7 +3,7 @@ const joinRouter=[
     path:'/member/Join',
     name: 'join',
     component : () => import('@/views/components/Join.vue')
-    }
+    },
 ]
 
 export default joinRouter;
