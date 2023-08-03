199d0c51b9e8bb71e26172881d86b46de1b8b6fb
ksc <ksc036@gmail.com>
2023-08-04 06:56:33 +0900

    fix publicPath loc수정

--- Frontend/jobterview/vue.config.js
+++ Frontend/jobterview/vue.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: '/testfe/',
   pluginOptions: {
-    publicPath: '/testfe/',
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
