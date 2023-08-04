41990f21111beea4c45a109d55487e433b38b557
ksc <ksc036@gmail.com>
2023-08-05 05:29:03 +0900

    fix testfe

--- Frontend/jobterview/src/config/config.js
+++ Frontend/jobterview/src/config/config.js
@@ -1,5 +1,5 @@
 const apiUrl = "http://localhost:8060";
-const apiUrlPrefix = "/test";
+const apiUrlPrefix = "/testfe";
 module.exports ={apiUrl,apiUrlPrefix};
 // export const apiKey = "YOUR_API_KEY";
 // export const maxResults = 10;
