b65fa1eb3e6c1b4f96075b388339e8f92a182e64
ksc <ksc036@gmail.com>
2023-08-08 17:52:11 +0900

    fix fe에 소켓연결

--- Frontend/jobterview/src/config/config.js
+++ Frontend/jobterview/src/config/config.js
@@ -2,5 +2,6 @@
 const apiUrl = "https://i9a701.p.ssafy.io/testbe";
 //prefix url 집에서 테스트시에는 /로함면됨.
 const apiUrlPrefix = "/testfe";
-
-module.exports = { apiUrl, apiUrlPrefix };
+// 시그널링 서버로 연결
+const socketUrl = "https://i9a701.p.ssafy.io"
+module.exports = { apiUrl, apiUrlPrefix,socketUrl};
