7d3395c04ad97203a881d12f8519c1aedf416b1a
ksc <ksc036@gmail.com>
2023-08-09 15:06:38 +0900

    add stop.sh

--- RTC/BE/stop.sh
+++ RTC/BE/stop.sh
@@ -1,4 +1,4 @@
-process=$(sudo lsof -i :4060 | awk '$1=="node"{print $2}')
+process=$(sudo lsof -i :4060 | awk '$1=="node"{print $2}'| head -n 1)
 if [ -n "$process" ]; then
     echo "포트 4060을 사용하는 프로세스($process)를 종료합니다."
     sudo kill "$process"
