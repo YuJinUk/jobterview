3764a2f98d99ac24926f97961f375045ff6bb7b3
ksc <ksc036@gmail.com>
2023-08-09 15:20:44 +0900

    config 설정 변경

--- Frontend/jobterview/src/config/config.js
+++ Frontend/jobterview/src/config/config.js
@@ -1,6 +1,6 @@
 //요청을 보낼 url => 백엔드 url
-//const apiUrl = "https://i9a701.p.ssafy.io/testbe";
-const apiUrl = "http://localhost:8060";
+const apiUrl = "https://i9a701.p.ssafy.io/testbe";
+// const apiUrl = "http://localhost:8060";
 
 //prefix url 집에서 테스트시에는 /로함면됨.
 const apiUrlPrefix = "/testfe";
