7ce321b0ce2607eb975547f532facecb22d0908c
ksc <ksc036@gmail.com>
2023-08-10 12:20:47 +0900

    add dev/prod 환경 설정파일 조정

--- Frontend/jobterview/src/config/config.js
+++ Frontend/jobterview/src/config/config.js
@@ -1,6 +1,5 @@
-//요청을 보낼 url => 백엔드 url
+//요청을 보낼 로컬에서는 localhost:8060 으로가고 master로 보내면 우리서버로 감
 const apiUrl = process.env.NODE_ENV === 'production' ? "https://i9a701.p.ssafy.io/testbe": "http://localhost:8060";
-// const apiUrl = "http://localhost:8060";
 
 //prefix url 집에서 테스트시에는 /로함면됨.
 const apiUrlPrefix = "/testfe";
