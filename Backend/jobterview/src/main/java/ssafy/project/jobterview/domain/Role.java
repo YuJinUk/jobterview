1c9b6e42ec681643b37bbc1e767588701e2f4774
LeeKyeongho <rudgh1366@naver.com>
2023-08-11 13:19:25 +0900

    fix) 백엔드ROLE 관련 수정 및 로그인 css진행중

--- Backend/jobterview/src/main/java/ssafy/project/jobterview/domain/Role.java
+++ Backend/jobterview/src/main/java/ssafy/project/jobterview/domain/Role.java
@@ -7,10 +7,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Role {
     ROLE_ADMIN("ROLE_ADMIN"), //관리자
-    ROLE_MEMBER("ROLE_MEMBER"), //이메일 인증 완료된 사용자
+    ROLE_SOCIAL("ROLE_SOCIAL"), // 소셜로 로그인한 사람
+    ROLE_LOCAL("ROLE_LOCAL"), // 로컬로 로그인한 사람
     ROLE_UNVERIFIED("ROLE_UNVERIFIED"), //이메일 인증이 안된 사용자
-    ROLE_REPORTED("ROLE_REPORTED"),
+    ROLE_REPORTED_SOCIAL("ROLE_REPORTED_SOCIAL"),
+    ROLE_REPORTED_LOCAL("ROLE_REPORTED_LOCAL"),
     ROLE_WITHDRAWN("ROLE_WITHDRAWN");
-
     private String description;
 }
