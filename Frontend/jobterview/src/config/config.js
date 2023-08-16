919afc14d4df9c65569d716c3146e4d9119a251b
ksc <ksc036@gmail.com>
2023-08-17 06:24:42 +0900

    prefix change

--- Frontend/jobterview/src/config/config.js
+++ Frontend/jobterview/src/config/config.js
@@ -2,7 +2,7 @@
 const apiUrl = process.env.NODE_ENV === 'production'? "https://i9a701.p.ssafy.io/testbe" : "http://localhost:8060";
 
 //prefix url 집에서 테스트시에는 /로함면됨.
-const apiUrlPrefix = "/testfe";
+const apiUrlPrefix = "/";
 
 // 시그널링 서버로 연결
 const socketUrl = "https://i9a701.p.ssafy.io"
